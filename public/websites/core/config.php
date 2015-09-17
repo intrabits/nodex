ec4020495f92990b0f2dbd344a171b7a8445fa52
Cedric <cedric.luckie@gmail.com>
2015-09-16 21:06:36 -0500

    Mejorar conexión a BD en PHP

--- public/websites/core/config.php
+++ public/websites/core/config.php
@@ -8,10 +8,13 @@ setlocale(LC_MONETARY, 'es_MX');
 $hoy=date('Y-m-d');
 $ahora=date('H:i:s');
 
+$string = file_get_contents("./../../../../config/config.json");
+$config = json_decode($string, true);
+
 if(!defined('DB_HOST')) define('DB_HOST', 'localhost'); // Database Host Name
-if(!defined('DB_USER')) define('DB_USER', 'root'); // Database Username
-if(!defined('DB_PASS')) define('DB_PASS', ''); // Database Password
-if(!defined('DB_NAME')) define('DB_NAME', 'mydb'); // Database Name
+if(!defined('DB_USER')) define('DB_USER', $config['db']['user']); // Database Username
+if(!defined('DB_PASS')) define('DB_PASS', $config['db']['password']); // Database Password
+if(!defined('DB_NAME')) define('DB_NAME', $config['db']['database']); // Database Name
 
 
 $db = new database(DB_HOST, DB_NAME, DB_USER,DB_PASS);
